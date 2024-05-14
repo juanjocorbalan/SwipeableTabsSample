ec760d5be48efcf27a787237ed6e7f985868cbef
Juanjo Corbalán <juanjocorbalan@gmail.com>
2024-05-14 10:46:30 +0200

    Initial Commit

---
+++ SwipeableTabsSample/SwipeableTabsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeableTabsSampleApp.swift
+//  SwipeableTabsSample
+//
+//  Created by Juanjo Corbalan on 14/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeableTabsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
